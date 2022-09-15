210003cb633678d3d8c36b09f1fb88c0c22e3a53
dimon4ay <craftman97@icloud.com>
2022-09-16 02:57:11 +0500

    fourthAttempt

--- js/render.js
+++ js/render.js
@@ -6,17 +6,19 @@ const newTemplate = template.querySelector('.picture');
 const photos = document.querySelector('.pictures');
 const factor = document.createDocumentFragment();
 
-const renderPictures = (posts) => {
-  posts.forEach((post) =>
-  {
-    const cloneOfPicture = newTemplate.cloneNode(true);
-    cloneOfPicture.querySelector('img').src = post.url;
-    cloneOfPicture.querySelector('.picture__likes').textContent = post.likes;
-    cloneOfPicture.querySelector('.picture__comments').textContent = post.comments.length;
+const renderPicture = ({url, likes, comments}) => {
+  const cloneOfPicture = newTemplate.cloneNode(true);
+  cloneOfPicture.querySelector('img').src = url;
+  cloneOfPicture.querySelector('.picture__likes').textContent = likes;
+  cloneOfPicture.querySelector('.picture__comments').textContent = comments.length;
 
-    factor.appendChild(cloneOfPicture);
-  });
+  factor.appendChild(cloneOfPicture);
+};
 
+const renderPictures = (pictures) => {
+  for (let i = 0; i < pictures.length; i++) {
+    renderPicture(pictures[i]);
+  }
   photos.appendChild(factor);
 };
 
