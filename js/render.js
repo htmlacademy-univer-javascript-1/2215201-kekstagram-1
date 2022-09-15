6de0c2a197ac47039107e618ea33e97e8ed1f045
dimon4ay <craftman97@icloud.com>
2022-09-16 02:41:44 +0500

    fourthAttempt

--- js/render.js
+++ js/render.js
@@ -1,25 +1,24 @@
-import {posts} from './main';
+import {generatePosts} from './util';
 
 const template = document.querySelector('#picture').content;
-const newPicture = template.querySelector('.picture');
+const newTemplate = template.querySelector('.picture');
+
 const photos = document.querySelector('.pictures');
+const factor = document.createDocumentFragment();
 
-const renderPicture = (url, amountOfLikes, amountOfComments) => {
-  const cloneOfPicture = newPicture.cloneNode(true);
-  const factor = document.createDocumentFragment();
+const renderPictures = (posts) => {
+  posts.forEach((post) =>
+  {
+    const cloneOfPicture = newTemplate.cloneNode(true);
+    cloneOfPicture.querySelector('img').src = post.url;
+    cloneOfPicture.querySelector('.picture__likes').textContent = post.likes;
+    cloneOfPicture.querySelector('.picture__comments').textContent = post.comments.length;
 
-  cloneOfPicture.querySelector('img').src = url;
-  cloneOfPicture.querySelector('.picture__likes').textContent = amountOfLikes;
-  cloneOfPicture.querySelector('.picture__comments').textContent = amountOfComments;
+    factor.appendChild(cloneOfPicture);
+  });
 
-  factor.appendChild(cloneOfPicture);
   photos.appendChild(factor);
 };
 
-const generateSomePictures = (blogs) => {
-  for (let i = 0; i < blogs.length; i++) {
-    renderPicture(blogs[i].url, blogs[i].likes, blogs[i].comments.length);
-  }
-};
+renderPictures(generatePosts(25));
 
-generateSomePictures(posts);
