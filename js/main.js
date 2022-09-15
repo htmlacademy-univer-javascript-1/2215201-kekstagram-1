549b511031c4b8f326e1cfe1c1b8cdf49f1e68c4
dimon4ay <craftman97@icloud.com>
2022-09-15 12:20:49 +0500

    secondAttempt

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 import {generatePosts} from './util';
 import {generateSomePictures} from './render';
 
-generateSomePictures();
-
-// Если я правильно понял, то данные для этого задания мы берём 'условно' рандомные.
-// До сих пор не совсем разобрался, может ли этот проект запуститься (через run 'main.js') на данном этапе или это будет
-//   доступно позже? (Например, для проверки коретного выполнения задания.)
-
 const posts = generatePosts(25);
 export {posts};
+
+generateSomePictures(posts);
--- js/render.js
+++ js/render.js
@@ -4,24 +4,19 @@ const photos = document.querySelector('.pictures');
 
 const renderPicture = (url, amountOfLikes, amountOfComments) => {
   const cloneOfPicture = newPicture.cloneNode(true);
-  const out = document.createDocumentFragment();
+  const factor = document.createDocumentFragment();
 
-  const newUrl = cloneOfPicture.querySelector('img');
-  newUrl.src = url;
+  cloneOfPicture.querySelector('img').src = url;
+  cloneOfPicture.querySelector('.picture__likes').textContent = amountOfLikes;
+  cloneOfPicture.querySelector('.picture__comments').textContent = amountOfComments;
 
-  const newLikes = cloneOfPicture.querySelector('.picture__likes');
-  newLikes.textContent = amountOfLikes;
-
-  const newComments = cloneOfPicture.querySelector('.picture__comments');
-  newComments.textContent = amountOfComments;
-
-  out.appendChild(cloneOfPicture);
-  photos.appendChild(out);
+  factor.appendChild(cloneOfPicture);
+  photos.appendChild(factor);
 };
 
-const generateSomePictures = () => {
-  for (let i = 0 ; i < 25; i++) {
-    renderPicture(`./photos/${i}.jpg`, i * 3, i * 1);
+const generateSomePictures = (posts) => {
+  for (let i = 0; i < posts.length; i++) {
+    renderPicture(posts[i].url, posts[i].likes, posts[i].comments.length);
   }
 };
 
