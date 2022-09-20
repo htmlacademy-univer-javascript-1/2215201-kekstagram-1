d02b61847aef4505c1055ca98fea0a562d037c5e
dimon4ay <craftman97@icloud.com>
2022-09-20 17:08:11 +0500

    Comments now deleting from hidden element.

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -3,7 +3,6 @@ const socCommentCount = document.querySelector('.social__comment-count');
 const comLoader = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 
-
 const closeOption = () => {
   bigPicture.classList.add('hidden');
   socCommentCount.classList.remove('hidden');
@@ -13,14 +12,46 @@ const closeOption = () => {
   bigPicture.querySelector('.social__comments').replaceChildren();
 };
 
+const escButtonPossibleAction = (keyEvent) => {
+  if (keyEvent.keyCode === 27) {
+    closeOption();
+    document.removeEventListener('keydown', escButtonPossibleAction);
+  }
+};
+
 const addClosingOption = () => {
   document.querySelector('.big-picture__cancel').addEventListener('click', () => {
     closeOption();
   });
-  document.addEventListener('keydown', (keyEvent) => {
-    if (keyEvent.keyCode === 27) {
-      closeOption();
-    }
+  document.addEventListener('keydown', escButtonPossibleAction);
+};
+
+const renderComment = ({avatar, message, name}) => {
+  const container = document.createDocumentFragment();
+
+  const tempLi = document.createElement('li');
+  tempLi.classList.add('social__comment');
+  container.append(tempLi);
+
+  const tempImg = document.createElement('img');
+  tempImg.classList.add('social__picture');
+  tempImg.src = avatar;
+  tempImg.alt = name;
+  tempImg.width = 35;
+  tempImg.height = 35;
+  container.querySelector('li').append(tempImg);
+
+  const tempP = document.createElement('p');
+  tempP.classList.add('social__text');
+  tempP.textContent = message;
+  container.querySelector('li').append(tempP);
+
+  bigPicture.querySelector('.social__comments').append(container);
+};
+
+const renderComments = (comments) => {
+  comments.forEach((comment) => {
+    renderComment(comment);
   });
 };
 
@@ -30,28 +61,7 @@ const renderBigPicture = ({url, description, likes, comments}) => {
   document.querySelector('.comments-count').textContent = comments.length;
   document.querySelector('.social__caption').textContent = description;
 
-  comments.forEach(({avatar, message, name}) => {
-    const container = document.createDocumentFragment();
-
-    const tempLi = document.createElement('li');
-    tempLi.classList.add('social__comment');
-    container.append(tempLi);
-
-    const tempImg = document.createElement('img');
-    tempImg.classList.add('social__picture');
-    tempImg.src = avatar;
-    tempImg.alt = name;
-    tempImg.width = 35;
-    tempImg.height = 35;
-    container.querySelector('li').append(tempImg);
-
-    const tempP = document.createElement('p');
-    tempP.classList.add('social__text');
-    tempP.textContent = message;
-    container.querySelector('li').append(tempP);
-
-    bigPicture.querySelector('.social__comments').append(container);
-  });
+  renderComments(comments);
 
   bigPicture.classList.remove('hidden');
   socCommentCount.classList.add('hidden');
